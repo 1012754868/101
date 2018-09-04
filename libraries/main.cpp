c3cdd494cac3577bf78ab69b28615a12048dc0a4
SaiWen Li <42888479+1012754868@users.noreply.github.com>
2018-09-04 14:20:17 +0800

    Update main.cpp
    Add some test to modify the functions

--- libraries/main.cpp
+++ libraries/main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
-
-// including the main header will ensure all required files are pulled in
+#include "maths.h"
 #include "main.h"
 
+using namespace std;
+
 int main() {
-  int total = sum(5, 6);  // sum function defined in maths.h
-  std::cout << "Sum = " << total << std::endl;
-  return 0;
+    int a;
+    int b;
+    cout<<"Please input two numbers"<<endl;
+    cin>>a>>b;
+    
+    cout<<"The sum of the two numbers are = ";
+    cout<<sum(a,b)<<endl;
+
+    cout<<"The product of the two numbers are = ";
+    cout<<product(a,b)<<endl;
+
+    max(a,b);
+
+    min(a,b);
+
+    return 0;
+
+
 }
