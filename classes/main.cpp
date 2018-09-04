5c05a7b48850ef961cbfb1c3d76a34de8189cd72
SaiWen Li <42888479+1012754868@users.noreply.github.com>
2018-09-04 14:48:10 +0800

    Update main.cpp

--- classes/main.cpp
+++ classes/main.cpp
@@ -1,15 +1,17 @@
-#include <iostream>
-// include the class header file in main
+#include <iostream> // include the class header file in main
 #include "Circle.h"
 
+using namespace std;
+
 int main() {
-  std::cout << "Enter circle radius (in metres): ";
-  float radius;
-  std::cin >> radius;
-  // create a circle object and use the public methods
-  Circle circle;
-  circle.set_radius(radius);
-  float area = circle.get_area();
-  std::cout << "The circle has an area of " << area << " m^2.\n";
-  return 0;
+    cout << "Enter circle radius (in metres): ";
+    float radius;
+    cin >> radius; // create a circle object and use the public methods
+    Circle circle;
+    circle.set_radius(radius);
+    float area = circle.get_area();
+    cout << "The circle has an area of " << area << " m^2.\n";
+
+    system("pause");
+    return 0;
 }
