e9c2c6b8148cca8628b187f2ac34afd35c673bba
SaiWen Li <42888479+1012754868@users.noreply.github.com>
2018-09-04 14:22:03 +0800

    Update maths.cpp
    add some function to update the maths's library

--- libraries/maths.cpp
+++ libraries/maths.cpp
@@ -1,3 +1,32 @@
+#include <iostream>
 #include "maths.h"
 
-int sum(int a, int b) { return a  + b; }
+using namespace std;
+
+int sum(int a, int b) {
+    return (a + b);
+}
+
+int product(int a, int b) {
+    return (a * b);
+}
+
+int max(int a, int b) {
+    if(a > b) {
+        cout<<"The max number is = "<<a<<endl;
+    }
+    else {
+        cout<<"The max number is = "<<b<<endl;
+    }
+    return 0;
+}
+
+int min(int a, int b) {
+    if(a < b) {
+        cout<<"The min number is = "<<a<<endl;
+    }
+    else {
+        cout<<"The min number is = "<<b<<endl;
+    }
+    return 0;
+}
